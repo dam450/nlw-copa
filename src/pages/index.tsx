fb848cdf0c278726d074de8a1fa069c0d78d094e
Dam450 <68165752+dam450@users.noreply.github.com>
2022-11-03 11:22:01 -0300

    web layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-// interface HomeProps {
-//   count: number
-// }
+interface HomeProps {
+  poolCount: number
+}
 
 import Image from 'next/image'
 import appPreviewImage from '../assets/app-nlw-copa-preview.png'
@@ -8,39 +8,52 @@ import logoImg from '../assets/logo.svg'
 import usersAvatar from '../assets/users-avatar-example.png'
 import iconCheck from '../assets/icon-check.svg'
 
-export default function Home() {
+export default function Home(props: HomeProps) {
   return (
-    <div>
-      <main>
+    <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
+      <main className='max-w-lg'>
         <Image src={logoImg} alt="Logo da NLW Copa"/>
-        <h1 className='text-white'>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
-        <div>
+        <h1 className='text-white mt-14 text-5xl font-bold leading-tight'>
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className='mt-10 flex items-center gap-2'>
           <Image src={usersAvatar} alt="Imagens de avatares de usuários" />
-          <strong>
-            <span>+12.592</span> pessoas já estão usando
+          <strong className='text-gray-100 font-bold text-xl'>
+            <span className='text-ignite-500'>+12.592</span> pessoas já estão usando
           </strong>
         </div>
 
-        <form>
-          <input type="text" name="" id="" required placeholder='Qual nome do seu bolão?'/>
-          <button type="submit" >Criar meu bolão</button>
+        <form className='mt-10 flex gap-2'>
+          <input 
+            type="text" name="" id="" required placeholder='Qual nome do seu bolão?'
+            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+          />
+          <button 
+            type="submit" 
+            className='bg-yellow-500 hover:bg-yellow-700 font-bold uppercase leading-4 px-6 py-4 rounded text-sm text-gray-950'
+          >Criar meu bolão</button>
         </form>
 
-        <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+        <p className='mt-4 max-w-sm text-gray-300 text-sm leading-relaxed'>
+          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+        </p>
 
-        <div>
-          <div>
+        <div className='mt-10 pt-10 border-t border-gray-600 text-gray-100 flex items-center justify-between' >
+          <div className='flex items-center gap-6'>
             <Image src={iconCheck} alt="" />
-            <div>
-              <span>+2.034</span>
+            <div className='flex flex-col'>
+              <span className='font-bold text-2xl'>+{props.poolCount}</span>
               <span>Bolões criados</span>
             </div>
           </div>
 
-          <div>
+          <div className='w-px h-14 bg-gray-600'/>
+
+          <div className='flex items-center gap-6'>
             <Image src={iconCheck} alt="" />
-            <div>
-              <span>+192.847</span>
+            <div className='flex flex-col'>
+              <span className='font-bold text-2xl'>+192.847</span>
               <span>Palpites enviados</span>
             </div>
           </div>
@@ -56,15 +69,15 @@ export default function Home() {
   )
 }
 
-// export const getServerSideProps = async () => {
-//   const response = await fetch('http://localhost:3333/pools/count')
-//   const data = await response.json()
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
 
-//   console.log(data)
+  console.log(data)
 
-//   return {
-//     props: {
-//       count: data.count,
-//     }
-//   }
-// }
+  return {
+    props: {
+      poolCount: data.count,
+    }
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,25 @@ module.exports = {
         sans: 'Roboto, sans-serif'
       },
 
+      backgroundImage: {
+        app: 'url(/bg-effects.png)'
+      },
+
       colors: {
         gray: {
-          900: '#121214'
+          100: '#E1E1E6',
+          300: '#8D8D99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214',
+          950: '#09090A'
+        },
+        ignite: {
+          500: '#129E57'
+        },
+        yellow: {
+          500: '#F7DD43',
+          700: '#CBB73F'
         }
       }
     }
