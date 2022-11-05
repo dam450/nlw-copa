0e9d1b58bd885b15e00c400d07686f55abdd0133
Dam450 <68165752+dam450@users.noreply.github.com>
2022-11-05 00:21:49 -0300

    create pools auth route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
 import { authRoutes } from './routes/auth'
 import { gameRoutes } from './routes/game'
@@ -14,6 +15,10 @@ async function bootstrap() {
 
   await fastify.register(cors, {
     origin: true
+  })
+
+  await fastify.register(jwt, {
+    secret: 'nlwcopa' //em produção isso precisa ser uma variável de ambiente
   })
 
   await fastify.register(authRoutes)
