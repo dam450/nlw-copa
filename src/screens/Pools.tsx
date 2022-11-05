7f14090f6c5218c96f59426604522ee3db81601e
Dam450 <68165752+dam450@users.noreply.github.com>
2022-11-05 15:24:39 -0300

    add routes navigation

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,10 +1,14 @@
 import { VStack, Icon } from 'native-base'
+import { Octicons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+
 import { Button } from '../components/Button'
 import { Header } from '../components/Header'
 
-import { Octicons } from '@expo/vector-icons'
 
 export function Pools() {
+  const { navigate } = useNavigation()
+  
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Meus bolões" />
@@ -18,9 +22,8 @@ export function Pools() {
       >
         <Button
           title="Buscar bolão por código"
-          leftIcon={
-            <Icon as={Octicons} name="search" color="black" size="md" />
-          }
+          leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
+          onPress={() => navigate('find')} 
         />
       </VStack>
     </VStack>
