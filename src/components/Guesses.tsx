4123dba349cf95d29dcd2c1cc749f904df6b311d
Dam450 <68165752+dam450@users.noreply.github.com>
2022-11-08 15:30:39 -0300

    create guesses component

--- src/components/Guesses.tsx
+++ src/components/Guesses.tsx
@@ -1,14 +1,62 @@
-import { Box } from 'native-base';
+import { useEffect, useState } from 'react'
+import { Box, FlatList, useToast } from 'native-base'
+import { Game, GameProps } from '../components/Game'
+
+import { api } from '../services/api'
 
 interface Props {
-  poolId: string;
+  poolId: string
 }
 
 export function Guesses({ poolId }: Props) {
+  const [isLoading, setIsLoading] = useState(true)
+  const [games, setGames] = useState([])
+  const [firstTeamPoints, setFirstTeamPoints] = useState('')
+    const [secondTeamPoints, setSecondTeamPoints] = useState('')
+
+  const toast = useToast()
+
+  async function fetchGames() {
+    try {
+      setIsLoading(true)
+
+      const response = await api.get(`/pools/${poolId}/games`)
+      setGames(response.data.games)
+      
+    } catch (error) {
+      console.error(error)
+
+      toast.show({
+        title: 'Não foi possível carregar os jogos',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchGames()
+  },[poolId])
 
   return (
-    <Box>
+    <FlatList
+      data={games}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => (
+        <Game
+          data={item}
+          setFirstTeamPoints={setFirstTeamPoints}
+          setSecondTeamPoints={setSecondTeamPoints}
+          key={item}
+          onGuessConfirm={() => null}
 
-    </Box>
-  );
+        />
+      )}
+    />
+
+    
+  )
 }
